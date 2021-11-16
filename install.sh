066b2d33fe1004a720d6e96d5d6dcdc8c00fa244
Wolf <zhao.haiyang@fruitsvending.com>
2021-11-16 15:28:19 +0800

    add user privilege checing

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+#Reference https://brezular.com/2021/04/01/docker-installation-on-vyos/
+
+user=$(id -u)
+docker_comp_ver='1.28.5'
+
+# Check if running as root
+[ "$user" != 0 ] && echo "Run script as root, exiting" && exit 1 
